922b14b93b178018938cfe9eff1e1f81754a7e4f
Frank Rodriguez <frank0631@gmail.com>
2018-06-10 00:34:53 +0000

    refactor muninn to make API calls in separate methods. add jackson & jersey for REST calls

--- src/main/java/com/frank0631/muninn/MuninnApplication.java
+++ src/main/java/com/frank0631/muninn/MuninnApplication.java
@@ -2,38 +2,60 @@ package com.frank0631.muninn;
 
 import com.frank0631.nidhogg.book.Book;
 import com.frank0631.nidhogg.echo.TEchoService;
-import org.apache.thrift.TException;
-import org.apache.thrift.protocol.TBinaryProtocol;
-import org.apache.thrift.protocol.TJSONProtocol;
-import org.apache.thrift.protocol.TProtocol;
-import org.apache.thrift.transport.THttpClient;
-import org.apache.thrift.transport.TSocket;
-import org.apache.thrift.transport.TTransport;
-import org.apache.thrift.transport.TTransportException;
 
-import java.lang.reflect.Field;
+import org.apache.thrift.*;
+import org.apache.thrift.protocol.*;
+import org.apache.thrift.transport.*;
+
+import javax.ws.rs.client.*;
+import javax.ws.rs.core.*;
+
 
 public class MuninnApplication {
-    private static String Message = "Hello";
-    private static String echoEndpoint = "/thrift/echo/echo/";
-
+    private static final String ADDRESS = "http://localhost";
     private static final int PORT = 9001;
-
+    private static String echoMessage = "Hello";
+    private static String echoThriftEndpoint = "/thrift/echo/echo/";
+    private static String echoRestEndpoint = "/api/echo/";
+    
     public static void main(String[] args) {
-        try {
+            restEcho();
+            thriftEcho();
+    }
 
-            TTransport transport = new THttpClient("http://localhost:" + PORT + echoEndpoint);
+    public static void thriftEcho(){
+        try {
+            TTransport transport = new THttpClient(ADDRESS+ ":" + PORT + echoThriftEndpoint);
             TProtocol protocol = new TJSONProtocol(transport);
             TEchoService.Client client = new TEchoService.Client(protocol);
             transport.open();
-
-            String echo = client.echo(Message);
+            String echo = client.echo(echoMessage);
             transport.close();
             System.out.println(echo);
 
         } catch (TTransportException e) {
             e.printStackTrace();
         } catch (TException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void restEcho(){
+        try{
+
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(ADDRESS+ ":" + PORT + echoRestEndpoint);
+        
+        Invocation.Builder invocationBuilder;
+        invocationBuilder = target.request(MediaType.APPLICATION_JSON);
+		Form form = new Form();
+        form.param("input", echoMessage);
+		
+        Response response = invocationBuilder.post(Entity.form(form));
+        System.out.println(response.getStatus());
+        System.out.println(response.readEntity(String.class));
+        
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
